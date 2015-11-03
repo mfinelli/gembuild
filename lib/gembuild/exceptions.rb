b43bd02e0cc7b039b91c3ddb43320618b2c51f4a
Mario Finelli <mario@finel.li>
2015-11-02 20:58:47 -0500

    Alphabetize exceptions

--- lib/gembuild/exceptions.rb
+++ lib/gembuild/exceptions.rb
@@ -1,15 +1,15 @@
 # encoding: utf-8
 
 module Gembuild
-  # Exception raised when no pkgname is specified.
-  class UndefinedPkgnameError < StandardError; end
-
-  # Exception raised when no gemname is specified.
-  class UndefinedGemNameError < StandardError; end
-
   # Exception raised when rubygems.org returns a 404 error.
   class GemNotFoundError < StandardError; end
 
   # Exception raised when a non-string pkgbuild is passed.
   class InvalidPkgbuildError < StandardError; end
+
+  # Exception raised when no gemname is specified.
+  class UndefinedGemNameError < StandardError; end
+
+  # Exception raised when no pkgname is specified.
+  class UndefinedPkgnameError < StandardError; end
 end
