723666e4542598eb74ab7486a5ecab204b8ec4bf
Mario Finelli <mario@finel.li>
2015-11-03 22:12:06 -0500

    Add rspec tests for prompt for confirmation

--- spec/gembuild_spec.rb
+++ spec/gembuild_spec.rb
@@ -1,13 +1,69 @@
 # encoding: utf-8
 
 describe Gembuild do
-  describe '#conf_file' do
+  describe '.conf_file' do
     it 'should return .gembuild in the home directory' do
       expect(Gembuild.conf_file).to eql(File.join(File.expand_path('~'), '.gembuild'))
     end
   end
 
-  describe '#configure' do
+  describe '.prompt_for_confirmation' do
+    context 'with unimportant input' do
+      it 'should output the detected value' do
+        expect(STDOUT).to receive(:puts).with('Detected "test", is this correct? (y/n)')
+        allow(Gembuild).to receive(:gets) { 'Yes' }
+        Gembuild.prompt_for_confirmation('test')
+      end
+
+      it 'should return a boolean' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'Yes' }
+        expect(Gembuild.prompt_for_confirmation('test')).to be_truthy
+      end
+    end
+
+    context 'with negative uppercase response' do
+      it 'should return false' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'No' }
+        expect(Gembuild.prompt_for_confirmation('test')).to eql(false)
+      end
+    end
+
+    context 'with negative lowercase response' do
+      it 'should return false' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'n' }
+        expect(Gembuild.prompt_for_confirmation('test')).to eql(false)
+      end
+    end
+
+    context 'with affermative uppercase response' do
+      it 'should return true' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'Y' }
+        expect(Gembuild.prompt_for_confirmation('test')).to eql(true)
+      end
+    end
+
+    context 'with affermative lowercase response' do
+      it 'should return true' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'yes' }
+        expect(Gembuild.prompt_for_confirmation('test')).to eql(true)
+      end
+    end
+
+    context 'with invalid response' do
+      it 'should return false' do
+        allow(STDOUT).to receive(:puts)
+        allow(Gembuild).to receive(:gets) { 'AAA' }
+        expect(Gembuild.prompt_for_confirmation('test')).to eql(false)
+      end
+    end
+  end
+
+  describe '.configure' do
     context 'with normal behavior' do
       it 'should respond to configure' do
         expect(Gembuild).to respond_to(:configure)
