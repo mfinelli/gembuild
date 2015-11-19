2a661ef55853d872721dfc79d68621e1bcd35203
Mario Finelli <mario@finel.li>
2015-11-18 22:39:23 -0500

    Copy gembuild module documentation

--- lib/gembuild/version.rb
+++ lib/gembuild/version.rb
@@ -16,6 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+# Create Arch Linux PKGBUILDs for ruby gems.
 module Gembuild
   VERSION = '0.1.0'
 end
