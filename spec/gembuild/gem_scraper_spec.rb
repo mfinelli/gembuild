0d9274c07aaff75e723f5f61311663c5e65fc62a
Mario Finelli <mario@finel.li>
2015-11-19 20:42:40 -0500

    Cleanup first half of gem scraper specs

--- spec/gembuild/gem_scraper_spec.rb
+++ spec/gembuild/gem_scraper_spec.rb
@@ -5,40 +5,44 @@ describe Gembuild::GemScraper do
     context 'with normal gem name' do
       let(:gem_scraper) { Gembuild::GemScraper.new('netrc') }
 
-      it 'should return a GemScraper instance' do
+      it 'returns a GemScraper instance' do
         expect(gem_scraper).to be_a(Gembuild::GemScraper)
       end
 
-      it 'should have a mechanize agent' do
+      it 'has a mechanize agent' do
         expect(gem_scraper.agent).to be_a(Mechanize)
       end
 
-      it 'should have the gemname set to the parameter' do
+      it 'has the gemname set to the parameter' do
         expect(gem_scraper.gemname).to eql('netrc')
       end
 
-      it 'should have the correct information url' do
-        expect(gem_scraper.url).to eql('https://rubygems.org/api/v1/versions/netrc.json')
+      it 'has the correct information url' do
+        url = 'https://rubygems.org/api/v1/versions/netrc.json'
+        expect(gem_scraper.url).to eql(url)
       end
 
-      it 'should have the correct dependency url' do
-        expect(gem_scraper.deps).to eql('https://rubygems.org/api/v1/dependencies?gems=netrc')
+      it 'has the correct dependency url' do
+        url = 'https://rubygems.org/api/v1/dependencies?gems=netrc'
+        expect(gem_scraper.deps).to eql(url)
       end
 
-      it 'should have the correct frontend url' do
+      it 'has the correct frontend url' do
         expect(gem_scraper.gem).to eql('https://rubygems.org/gems/netrc')
       end
     end
 
     context 'with no gemname' do
-      it 'should raise an error' do
-        expect { Gembuild::GemScraper.new(nil) }.to raise_exception(Gembuild::UndefinedGemNameError)
+      it 'raises an error' do
+        ex = Gembuild::UndefinedGemNameError
+        expect { Gembuild::GemScraper.new(nil) }.to raise_exception(ex)
       end
     end
 
     context 'with empty gemname' do
-      it 'should raise an error' do
-        expect { Gembuild::GemScraper.new('') }.to raise_exception(Gembuild::UndefinedGemNameError)
+      it 'raises an error' do
+        ex = Gembuild::UndefinedGemNameError
+        expect { Gembuild::GemScraper.new('') }.to raise_exception(ex)
       end
     end
   end
@@ -47,15 +51,18 @@ describe Gembuild::GemScraper do
     context 'with normal gem' do
       let(:gem_scraper) { Gembuild::GemScraper.new('netrc') }
 
-      it 'should return a Hash' do
+      it 'returns a Hash' do
         VCR.use_cassette('gem_scraper_versions_netrc') do
           expect(gem_scraper.query_latest_version).to be_a(Hash)
         end
       end
 
-      it 'should return the correct version' do
+      it 'returns the correct version' do
         VCR.use_cassette('gem_scraper_versions_netrc') do
-          expect(gem_scraper.query_latest_version[:sha]).to eql('de1ce33da8c99ab1d97871726cba75151113f117146becbe45aa85cb3dabee3f')
+          sha = 'de1ce33da8c99ab1d97871726cba7515' \
+                '1113f117146becbe45aa85cb3dabee3f'
+
+          expect(gem_scraper.query_latest_version[:sha]).to eql(sha)
         end
       end
     end
@@ -63,13 +70,13 @@ describe Gembuild::GemScraper do
     context 'with a prerelease gem' do
       let(:gem_scraper) { Gembuild::GemScraper.new('pry') }
 
-      it 'should return a Hash' do
+      it 'returns a Hash' do
         VCR.use_cassette('gem_scraper_versions_pry') do
           expect(gem_scraper.query_latest_version).to be_a(Hash)
         end
       end
 
-      it 'should return the correct version' do
+      it 'returns the correct version' do
         VCR.use_cassette('gem_scraper_versions_pry') do
           expect(gem_scraper.query_latest_version[:number]).to eql('0.10.3')
         end
@@ -79,9 +86,10 @@ describe Gembuild::GemScraper do
     context 'with gem that doesn\'t exist' do
       let(:gem_scraper) { Gembuild::GemScraper.new('asdfg') }
 
-      it 'should raise an exception' do
+      it 'raises an exception' do
         VCR.use_cassette('gem_scraper_versions_asdfg') do
-          expect { gem_scraper.query_latest_version }.to raise_exception(Gembuild::GemNotFoundError)
+          ex = Gembuild::GemNotFoundError
+          expect { gem_scraper.query_latest_version }.to raise_exception(ex)
         end
       end
     end
@@ -90,18 +98,23 @@ describe Gembuild::GemScraper do
   describe '#get_version_from_response' do
     context 'with normal gem' do
       let(:gem_scraper) { Gembuild::GemScraper.new('netrc') }
-      let(:results) {
+
+      let(:results) do
         VCR.use_cassette('gem_scraper_versions_netrc') do
           gem_scraper.query_latest_version
         end
-      }
-
-      it 'should return a gem version' do
-        expect(gem_scraper.get_version_from_response(results)).to be_a(Gem::Version)
       end
 
-      it 'should be the correct version' do
-        expect(gem_scraper.get_version_from_response(results).to_s).to eql('0.11.0')
+      let(:version_from_results) do
+        gem_scraper.get_version_from_response(results)
+      end
+
+      it 'returns a gem version' do
+        expect(version_from_results).to be_a(Gem::Version)
+      end
+
+      it 'is the correct version' do
+        expect(version_from_results.to_s).to eql('0.11.0')
       end
     end
   end
@@ -109,18 +122,28 @@ describe Gembuild::GemScraper do
   describe '#format_description_from_response' do
     context 'with a normal gem' do
       let(:gem_scraper) { Gembuild::GemScraper.new('netrc') }
-      let(:results) {
+
+      let(:results) do
         VCR.use_cassette('gem_scraper_versions_netrc') do
           gem_scraper.query_latest_version
         end
-      }
-
-      it 'should return a string' do
-        expect(gem_scraper.format_description_from_response(results)).to be_a(String)
       end
 
-      it 'should have the correct description' do
-        expect(gem_scraper.format_description_from_response(results)).to eql('This library can read and update netrc files, preserving formatting including comments and whitespace.')
+      let(:description_from_results) do
+        gem_scraper.format_description_from_response(results)
+      end
+
+      let(:expected_description) do
+        'This library can read and update netrc files, preserving ' \
+        'formatting including comments and whitespace.'
+      end
+
+      it 'returns a string' do
+        expect(description_from_results).to be_a(String)
+      end
+
+      it 'has the correct description' do
+        expect(description_from_results).to eql(expected_description)
       end
     end
 
