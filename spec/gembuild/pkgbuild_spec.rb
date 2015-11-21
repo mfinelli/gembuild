b8ea13dbfcd41371292f9612a3d4c762e037ebf6
Mario Finelli <mario@finel.li>
2015-11-20 21:36:57 -0500

    Fix initialize test code style

--- spec/gembuild/pkgbuild_spec.rb
+++ spec/gembuild/pkgbuild_spec.rb
@@ -9,139 +9,145 @@ describe Gembuild::Pkgbuild do
 
   describe '#initialize' do
     context 'with normal gem name' do
-      let(:pkgbuild) { Gembuild::Pkgbuild.new('mechanize') }
+      let(:pkgbuild) { described_class.new('mechanize') }
 
-      it 'should be a Pkgbuild' do
-        expect(pkgbuild).to be_a(Gembuild::Pkgbuild)
+      it 'is a Pkgbuild' do
+        expect(pkgbuild).to be_a(described_class)
       end
 
-      it 'should have the correct gemname' do
+      it 'has the correct gemname' do
         expect(pkgbuild.gemname).to eql('mechanize')
       end
 
-      it 'should have the correct pkgname' do
+      it 'has the correct pkgname' do
         expect(pkgbuild.pkgname).to eql('ruby-mechanize')
       end
 
-      it 'should have the correct checksum type' do
+      it 'has the correct checksum type' do
         expect(pkgbuild.checksum_type).to eql('sha256')
       end
 
-      it 'should have an architecture array' do
+      it 'has an architecture array' do
         expect(pkgbuild.arch).to be_a(Array)
       end
 
-      it 'should have any architecture' do
+      it 'has any architecture' do
         expect(pkgbuild.arch).to eql(['any'])
       end
 
-      it 'should have a make dependencies array' do
+      it 'has a make dependencies array' do
         expect(pkgbuild.makedepends).to be_a(Array)
       end
 
-      it 'should have the rubygems make dependency' do
+      it 'has the rubygems make dependency' do
         expect(pkgbuild.makedepends).to eql(['rubygems'])
       end
 
-      it 'should have a dependencies array' do
+      it 'has a dependencies array' do
         expect(pkgbuild.depends).to be_a(Array)
       end
 
-      it 'should have the ruby dependency' do
+      it 'has the ruby dependency' do
         expect(pkgbuild.depends).to eql(['ruby'])
       end
 
-      it 'should have a source array' do
+      it 'has a source array' do
         expect(pkgbuild.source).to be_a(Array)
       end
 
-      it 'should have the correct source' do
-        expect(pkgbuild.source).to eql(['https://rubygems.org/downloads/$_gemname-$pkgver.gem'])
+      it 'has the correct source' do
+        url = 'https://rubygems.org/downloads/$_gemname-$pkgver.gem'
+        expect(pkgbuild.source).to eql([url])
       end
 
-      it 'should have a no extract array' do
+      it 'has a no extract array' do
         expect(pkgbuild.noextract).to be_a(Array)
       end
 
-      it 'should have the correct no extract array' do
+      it 'has the correct no extract array' do
         expect(pkgbuild.noextract).to eql(['$_gemname-$pkgver.gem'])
       end
 
-      it 'should have an options array' do
+      it 'has an options array' do
         expect(pkgbuild.options).to be_a(Array)
       end
 
-      it 'should have the correct options' do
+      it 'has the correct options' do
         expect(pkgbuild.options).to eql(['!emptydirs'])
       end
 
-      it 'should have no checksum' do
+      it 'has no checksum' do
         expect(pkgbuild.checksum).to be_nil
       end
 
-      it 'should have no maintainer' do
+      it 'has no maintainer' do
         expect(pkgbuild.maintainer).to be_nil
       end
 
-      it 'should have no contributor' do
+      it 'has no contributor' do
         expect(pkgbuild.contributor).to eql([])
       end
 
-      it 'should have no description' do
+      it 'has no description' do
         expect(pkgbuild.description).to be_nil
       end
 
-      it 'should have no epoch' do
+      it 'has no epoch' do
         expect(pkgbuild.epoch).to be_nil
       end
 
-      it 'should have no license' do
+      it 'has no license' do
         expect(pkgbuild.license).to be_nil
       end
 
-      it 'should have no pkgrel' do
+      it 'has no pkgrel' do
         expect(pkgbuild.pkgrel).to be_nil
       end
 
-      it 'should have no pkgver' do
+      it 'has no pkgver' do
         expect(pkgbuild.pkgver).to be_nil
       end
 
-      it 'should have no url' do
+      it 'has no url' do
         expect(pkgbuild.url).to be_nil
       end
     end
 
     context 'with string pkgbuild' do
-      it 'should be a Pkgbuild' do
-        expect(Gembuild::Pkgbuild.new('mechanize', '')).to be_a(Gembuild::Pkgbuild)
+      it 'is a Pkgbuild' do
+        expect(described_class.new('mechanize', '')).to be_a(described_class)
       end
     end
 
     context 'with empty string pkgbuild' do
-      it 'should not call parse_existing_pkgbuild' do
-        expect_any_instance_of(Gembuild::Pkgbuild).to_not receive(:parse_existing_pkgbuild)
-        Gembuild::Pkgbuild.new('mina', '')
+      it 'doesn\'t call parse_existing_pkgbuild' do
+        method = :parse_existing_pkgbuild
+        expect_any_instance_of(described_class).to_not receive(method)
+        described_class.new('mina', '')
       end
     end
 
     context 'with nil pkgbuild' do
-      it 'should not call parse_existing_pkgbuild' do
-        expect_any_instance_of(Gembuild::Pkgbuild).to_not receive(:parse_existing_pkgbuild)
-        Gembuild::Pkgbuild.new('mina', nil)
+      it 'does not call parse_existing_pkgbuild' do
+        method = :parse_existing_pkgbuild
+        expect_any_instance_of(described_class).to_not receive(method)
+        described_class.new('mina', nil)
       end
     end
 
     context 'with non-nil, non-empty pkgbuild' do
-      it 'should call parse_existing_pkgbuild' do
-        expect_any_instance_of(Gembuild::Pkgbuild).to receive(:parse_existing_pkgbuild).with('TEST')
-        Gembuild::Pkgbuild.new('mina', 'TEST')
+      it 'calls parse_existing_pkgbuild' do
+        method = :parse_existing_pkgbuild
+        expect_any_instance_of(described_class).to receive(method)
+          .with('TEST')
+        described_class.new('mina', 'TEST')
       end
     end
 
     context 'with non-string pkgbuild' do
-      it 'should raise an exception' do
-        expect { Gembuild::Pkgbuild.new('mechanize', {}) }.to raise_exception(Gembuild::InvalidPkgbuildError)
+      it 'raises an exception' do
+        ex = Gembuild::InvalidPkgbuildError
+        expect { described_class.new('mechanize', {}) }.to raise_exception(ex)
       end
     end
   end
