4708363cfe3fb17847b6d0d956b8a10f3085a580
Mario Finelli <mario@finel.li>
2015-11-21 11:02:26 -0500

    Use single quotes instead of double quotes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ Coveralls.wear!
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.cassette_library_dir = 'spec/fixtures/cassettes'
   config.hook_into :webmock
 end
 
